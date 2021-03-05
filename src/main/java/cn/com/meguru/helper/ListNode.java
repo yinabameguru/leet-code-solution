7b9df8940367f19ee88a87e115cd820648945d69
meguru <meguru.yinaba@gmail.com>
2021-03-05 11:43:32 +0800

    删除链表中倒数第n个节点

---
+++ src/main/java/cn/com/meguru/helper/ListNode.java
@@ -0,0 +1,23 @@
+package cn.com.meguru.helper;
+
+/**
+ * @author jiaoziang
+ * @date 2021/3/5
+ **/
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
