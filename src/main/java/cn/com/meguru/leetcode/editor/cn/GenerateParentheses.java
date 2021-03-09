241bb38d7a9ac458e892296b95a4903a641247e7
meguru <meguru.yinaba@gmail.com>
2021-03-09 22:52:43 +0800

    括号生成-动态规划

--- src/main/java/cn/com/meguru/leetcode/editor/cn/GenerateParentheses.java
+++ src/main/java/cn/com/meguru/leetcode/editor/cn/GenerateParentheses.java
@@ -32,18 +32,58 @@
 package cn.com.meguru.leetcode.editor.cn;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GenerateParentheses {
     public static void main(String[] args) {
         GenerateParentheses mainClass = new GenerateParentheses();
         Solution solution = mainClass.new Solution();
-        solution.generateParenthesis(3);
+        solution.generateParenthesis(4);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
+        Map<Integer, List<String>> data = new HashMap<>();
+
+        //动态规划
         public List<String> generateParenthesis(int n) {
+            if (n == 1) {
+                List<String> parentheses = new ArrayList<String>();
+                parentheses.add("()");
+                return parentheses;
+            }
+            List<String> pre = generateParenthesis(n - 1);
+            data.put(n - 1, pre);
+            List<String> parentheses = new ArrayList<>();
+
+            for (int i = 0; i <= n - 1; i++) {
+                List<String> left = data.get(i), right = data.get(n - 1 - i);
+                if (left == null) {
+                    for (String r : right) {
+                        parentheses.add("()" + r);
+                    }
+                    continue;
+                }
+                for (String l : left) {
+                    String val = "(" + l + ")";
+                    if (right == null) {
+                        parentheses.add(val);
+                        continue;
+                    }
+                    for (String r : right) {
+                        parentheses.add(val + r);
+                    }
+                }
+            }
+            return parentheses;
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+        //回溯
+        public List<String> generateParenthesis1(int n) {
             List<String> res = new ArrayList<>();
             generateParenthesis(res, 0, 0, 0, n, new StringBuilder());
             return res;
