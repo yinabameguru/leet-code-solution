bc8dfc907249a7e613b935258dc9f4705e2983d4
meguru <meguru.yinaba@gmail.com>
2021-03-09 15:24:52 +0800

    合并两个有序链表

---
+++ src/main/java/cn/com/meguru/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -0,0 +1,104 @@
+//合并两个有序链表
+
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [], l2 = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列 
+// 
+// Related Topics 递归 链表 
+// 👍 1580 👎 0
+
+
+package cn.com.meguru.leetcode.editor.cn;
+
+import cn.com.meguru.helper.ListNode;
+
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        MergeTwoSortedLists mainClass = new MergeTwoSortedLists();
+        Solution solution = mainClass.new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = null, cur = null;
+        if (l1.val < l2.val) {
+            head = l1;
+            l1 = l1.next;
+        } else {
+            head = l2;
+            l2 = l2.next;
+        }
+        cur = head;
+        merge(cur, l1, l2);
+        return head;
+    }
+
+    private void merge(ListNode cur, ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            cur.next = l2;
+            return;
+        }
+        if (l2 == null) {
+            cur.next = l1;
+            return;
+        }
+        if (l1.val < l2.val) {
+            cur.next = l1;
+            l1 = l1.next;
+        } else {
+            cur.next = l2;
+            l2 = l2.next;
+        }
+        merge(cur.next, l1, l2);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
