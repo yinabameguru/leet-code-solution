8784e7764d7711c7c956ba5f4822238f8319a006
yinabameguru <764904418@qq.com>
2022-08-28 18:23:08 +0800

    172. 阶乘后的零

---
+++ src/main/java/cn/com/meguru/leetcode/editor/cn/FactorialTrailingZeroes.java
@@ -0,0 +1,71 @@
+//阶乘后的零
+
+//给定一个整数 n ，返回 n! 结果中尾随零的数量。 
+//
+// 提示 n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 3
+//输出：0
+//解释：3! = 6 ，不含尾随 0
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 5
+//输出：1
+//解释：5! = 120 ，有一个尾随 0
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：n = 0
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= n <= 10⁴ 
+// 
+//
+// 
+//
+// 进阶：你可以设计并实现对数时间复杂度的算法来解决此问题吗？ 
+//
+// Related Topics 数学 👍 714 👎 0
+
+
+package cn.com.meguru.leetcode.editor.cn;
+
+public class FactorialTrailingZeroes {
+    public static void main(String[] args) {
+        FactorialTrailingZeroes mainClass = new FactorialTrailingZeroes();
+        Solution solution = mainClass.new Solution();
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int trailingZeroes(int n) {
+        return trailingZeroes(n, 5, 5);
+    }
+
+    private int trailingZeroes(int n, int p, int q) {
+        if (n < p) {
+            return 0;
+        }
+        int res = n / p + trailingZeroes(n, p * q, q);
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
